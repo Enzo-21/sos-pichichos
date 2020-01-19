2bac2b0afd37c5d903fdd4d925f182f80ab82099
Enzo-21 <enzo21barrera@gmail.com>
2020-01-19 16:28:01 -0300

    THIRD  try

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoDbUrl: 'mongodb+srv://enzo_21:3oGazCH7V1eKMilJ@sos-pichichos-cluster-0npwj.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    port: process.env.port || 3000,
+    port: process.env.PORT,
     globalVariables: (req, res, next) => {
         res.locals.success_message = req.flash('success-message');
         res.locals.error_message = req.flash('error-message');
